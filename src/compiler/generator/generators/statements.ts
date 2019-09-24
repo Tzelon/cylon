24c8ecc006b65660fb8d22974102f4ed00dfff6b
Tzelon Machluf <tzelonm@wix.com>
2019-09-24 17:34:48 +0300

    placeholders

--- src/compiler/generator/generators/statements.ts
+++ src/compiler/generator/generators/statements.ts
@@ -1,4 +1,14 @@
-import { ReturnStatement, VarStatement, DefStatement } from '../../NodesTypes';
+import {
+  ReturnStatement,
+  VarStatement,
+  DefStatement,
+  IfStatement,
+  BreakStatement,
+  CallStatement,
+  FailStatement,
+  LetStatement,
+  LoopStatement,
+} from '../../NodesTypes';
 import Printer from '../printer';
 
 export function VarStatement(this: Printer, node: VarStatement) {
@@ -31,6 +41,25 @@ export function DefStatement(this: Printer, node: DefStatement) {
 
 export function ReturnStatement(this: Printer, node: ReturnStatement) {
   this.word('return');
-  this.print(node.zeroth, node)
-  this.semicolon()
+  this.print(node.zeroth, node);
+  this.semicolon();
+}
+
+export function IfStatement(this: Printer, node: IfStatement) {
+  throw 'Need to implement IfStatement';
+}
+export function BreakStatement(this: Printer, node: BreakStatement) {
+  throw 'Need to implement BreakStatement';
+}
+export function CallStatement(this: Printer, node: CallStatement) {
+  throw 'Need to implement CallStatement';
+}
+export function FailStatement(this: Printer, node: FailStatement) {
+  throw 'Need to implement FailStatement';
+}
+export function LetStatement(this: Printer, node: LetStatement) {
+  throw 'Need to implement LetStatement';
+}
+export function LoopStatement(this: Printer, node: LoopStatement) {
+  throw 'Need to implement LoopStatement';
 }
